8c557a9ab18432bed8390811fcdd4f84be12f42e
Cintia Garcia Garces <cintia.garcia.garces@gmail.com>
2021-05-20 17:07:59 +0200

    add build

---
+++ client/build/precache-manifest.f7a69108d33efe51d617184f6916bbb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "301ed5cc6c0274540be3",
+    "url": "/static/css/main.15993d16.chunk.css"
+  },
+  {
+    "revision": "301ed5cc6c0274540be3",
+    "url": "/static/js/main.e22c96c1.chunk.js"
+  },
+  {
+    "revision": "b3952c23394853283555",
+    "url": "/static/js/runtime~main.dc611263.js"
+  },
+  {
+    "revision": "7a06e1ee49da2f7c23fd",
+    "url": "/static/css/2.c2b762d8.chunk.css"
+  },
+  {
+    "revision": "7a06e1ee49da2f7c23fd",
+    "url": "/static/js/2.8857e4c5.chunk.js"
+  },
+  {
+    "revision": "227be4ff931475e4bd59",
+    "url": "/static/js/3.a08d5cb2.chunk.js"
+  },
+  {
+    "revision": "9ec8af4b07c121914c64a0af9f9417d3",
+    "url": "/index.html"
+  }
+];
