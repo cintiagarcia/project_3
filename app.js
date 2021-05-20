d1f7e89d3ba35256e41ae9c1adf1ae6f5e9442cb
Cintia Garcia Garces <cintia.garcia.garces@gmail.com>
2021-05-20 17:11:47 +0200

    update app.js

--- app.js
+++ app.js
@@ -96,6 +96,21 @@ app.use("/api/equipments", equipments);
 const auth = require("./routes/auth");
 app.use("/api/auth", auth);
 
+//deployment 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
