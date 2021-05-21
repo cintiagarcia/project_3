31146c48ea0d530955a15cb1d02ac426ab79107c
Cintia Garcia Garces <cintia.garcia.garces@gmail.com>
2021-05-21 10:58:37 +0200

    fronted

---
+++ client/build/precache-manifest.d454d17b5dc1067bb0fbbdd83debb0d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb3eebe2c7dc2c480ede",
+    "url": "/static/css/main.5386af27.chunk.css"
+  },
+  {
+    "revision": "fb3eebe2c7dc2c480ede",
+    "url": "/static/js/main.2e29d149.chunk.js"
+  },
+  {
+    "revision": "b3952c23394853283555",
+    "url": "/static/js/runtime~main.dc611263.js"
+  },
+  {
+    "revision": "139d395be196cfa7eb83",
+    "url": "/static/css/2.c2b762d8.chunk.css"
+  },
+  {
+    "revision": "139d395be196cfa7eb83",
+    "url": "/static/js/2.aba25573.chunk.js"
+  },
+  {
+    "revision": "227be4ff931475e4bd59",
+    "url": "/static/js/3.a08d5cb2.chunk.js"
+  },
+  {
+    "revision": "3d3d23a986d6d2ef1aa2a40ca5626988",
+    "url": "/index.html"
+  }
+];
